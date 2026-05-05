af4e6d0164d81b08eda8910302e7caec9b899992
Sr-L <luisanfincias@gmail.com>
2026-05-05 20:36:36 +0200

    feat(tasks): add complete and delete task use cases

---
+++ src/contexts/tasks/application/delete.task.use-case.js
@@ -0,0 +1,23 @@
+export class DeleteTaskUseCase {
+  constructor(taskRepository) {
+    this.taskRepository = taskRepository;
+  }
+
+  async execute({ taskId, userId }) {
+    const task = await this.taskRepository.findById(taskId);
+
+    if (!task) {
+      const err = new Error('Task not found');
+      err.status = 404;
+      throw err;
+    }
+
+    if (task.userId !== userId) {
+      const err = new Error('Forbidden');
+      err.status = 403;
+      throw err;
+    }
+
+    await this.taskRepository.delete(taskId);
+  }
+}
