48978822cdb37f8efeab3e95669947ebadf5a417
Sr-L <luisanfincias@gmail.com>
2026-05-05 20:34:41 +0200

    chore: init project structure and dependencies

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+export default {
+  testEnvironment: 'node',
+  transform: {},
+  extensionsToTreatAsEsm: [],
+  testMatch: ['**/tests/**/*.test.js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/server.js',
+    '!src/shared/infrastructure/database/mongoose.connection.js'
+  ],
+  coverageThresholds: {
+    global: {
+      lines: 80
+    }
+  },
+  coverageReporters: ['text', 'lcov']
+};
